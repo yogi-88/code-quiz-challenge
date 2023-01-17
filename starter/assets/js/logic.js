ff2ee3c4b16c7aad96506a21d38dbd0cd5a3c60a
Yogita Negi <ynegi6@gmail.com>
2023-01-17 22:33:56 +0000

    Function created to generate questions and choices added into it

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -17,17 +17,45 @@ function startQuiz() {
     // hide start screen
     var startScreenEl = document.getElementById("start-screen");
     startScreenEl.setAttribute("class", "hide");
-  
+
     // un-hide questions section
     questionsEl.removeAttribute("class");
-  
+
     // start timer
     timerId = setInterval(clockTick, 1000);
-  
+
     // show starting time
     timerEl.textContent = time;
-  
+
     getQuestion();
-  }
+}
+
+function getQuestion() {
+    // get current question object from array
+    var currentQuestion = questions[currentQuestionIndex];
+
+    // update title with current question
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+
+    // clear out any old question choices
+    choicesEl.innerHTML = "";
+
+    // loop over choices
+    currentQuestion.choices.forEach(function (choice, i) {
+        // create new button for each choice
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
+
+        choiceNode.textContent = i + 1 + ". " + choice;
+
+        // attach click event listener to each choice
+        choiceNode.onclick = questionClick;
+
+        // display on the page
+        choicesEl.appendChild(choiceNode);
+    });
+}
 
 
