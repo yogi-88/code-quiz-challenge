bfa3295deec438064401048486627b3f24d98460
Yogita Negi <ynegi6@gmail.com>
2023-01-17 22:51:54 +0000

    Clocktick function created , if user runs out of time quiz ends

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -102,3 +102,14 @@ function quizEnd() {
     //hide questions section
     questionsEl.setAttribute("class", "hide");
 }
+
+function clockTick() {
+    //update time
+    time--;
+    timerEl.textContent = time;
+
+    //check if user ran out of time
+    if (time <= 0) {
+        quizEnd();
+    }
+}
