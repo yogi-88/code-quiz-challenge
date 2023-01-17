70e342608357c80e7c86f60c5e2c69506847d314
Yogita Negi <ynegi6@gmail.com>
2023-01-17 22:24:29 +0000

    All Dom selectors required are assigned to variables

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,13 @@
+// Prepare all selector that we might need to point to the html element
+var startButton = document.querySelector('#start');
+var questionsEl = document.querySelector('#questions');
+var choicesEl = document.querySelector('#choices');
+var timerEl = document.querySelector('#time');
+var submitBtn = document.querySelector('#submit');
+var initialsEl = document.querySelector('#initials');
+var feedbackEl = document.querySelector('#feedback');
+var startScreenEl = document.getElementById("start-screen");
+
+
+
+
