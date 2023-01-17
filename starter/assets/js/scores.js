eb05efe9ec8d757ed409ad44fd8bedeb1f4440db
Yogita Negi <ynegi6@gmail.com>
2023-01-17 23:11:47 +0000

    Score.js file added function printHighScores to print high score

---
+++ starter/assets/js/scores.js
@@ -0,0 +1,20 @@
+function printHighscores() {
+    //either get scores from localstorage or set to empty array
+    var highScores = JSON.parse(window.localStorage.getItem("highScores")) || [];
+
+    // sort highScores by score property in descending order
+    highScores.sort(function(a,b) {
+        return b.score - a.score;
+    });
+
+    highScores.forEach(function(score) {
+        // create li tag for each high score
+        var liTag = document.createElement("li");
+        liTag.textContent = score.initials + " - " + score.score;
+
+        //display on page
+        var olEL = document.getElementById("highScores");
+        olEL.appendChild(liTag);
+
+    });
+}
