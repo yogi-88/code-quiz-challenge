caf26f858390db419605a9bcdada41c36d3173b4
Yogita Negi <ynegi6@gmail.com>
2023-01-17 21:51:21 +0000

    variable question created with title, choices and answers option in question.js file which would be further used in logic.js file

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,25 @@
+// Prepare the questions in question.js file
+
+var questions = [
+    {
+        title : "Commonly used data types Do NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title : "Arrays in Javascript can be used to store _____",
+        choices: ["numbers and strings", "other arrays", "booleans", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        title : "String values must be enclosed within_______ when being assigned to variables",
+        choices: ["commas", "curly brackets", "quotes", "paranthesis"],
+        answer: "quotes"
+    },
+    {
+        title : "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal/bash", "for loop", "console.log"],
+        answer: "console.log"
+    },
+
+]
